2037560c91fff95002c2628e9f96359bff7ce79f
Weijun Yin <gw9812@gmail.com>
2015-12-30 10:50:19 +0800

    impl Phoneix.Param protocol; custom Ecto type

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,13 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
 
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
+
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -23,6 +26,29 @@ defmodule Rumbl.Video do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
-    |> foreign_key_constraint(:category_id)
+    |> slugify_title()
+    |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugfy(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugfy(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/, "-")
   end
 end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  
+  def to_param(%{id: id, slug: slug}) do
+    "#{id}-#{slug}"
+  end
+end
+
