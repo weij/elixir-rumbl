44242ebf85b614a318af0116bdb14c0aa639f5d0
Weijun Yin <gw9812@gmail.com>
2016-02-29 17:28:55 +0800

    using last seen annotaion id to handle disconnects situation

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -34,12 +34,15 @@ let Video = {
       Player.seekTo(seconds)
     })
 
-    vidChannel.on("new_annotation", (resp) => {         
-        this.renderAnnotation(msgContainer, resp)
+    vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id         
+      this.renderAnnotation(msgContainer, resp)
     }) 
 
     vidChannel.join()
     .receive("ok", (resp) => {
+      let ids = resp.annotations.map( ann => ann.id)
+      vidChannel.params.last_seen_id = Math.max(...ids)
       this.scheduleMessages(msgContainer, resp.annotations)
     })
     .receive("error", reason => console.log("join failed", reason) )
