99d9237b31c8ad1d75641c44f6872576232c8e01
Weijun Yin <gw9812@gmail.com>
2016-02-29 16:49:46 +0800

    when join a channel it returns last 20 messages in that channel

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -1,11 +1,20 @@
 defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
+  alias Rumbl.AnnotationView
 
   def join("videos:" <> video_id, _params, socket) do
-    case Integer.parse(video_id) do
-      {int, _} -> {:ok, assign(socket, :video_id, int)}
-      _ -> {:error}
-    end
+    video_id = String.to_integer(video_id)
+    video = Repo.get!(Rumbl.Video, video_id)
+
+    annotations = Repo.all(
+      from a in assoc(video, :annotations),
+        order_by: [desc: a.at],
+        limit: 20,
+        preload: [:user]
+    )
+
+    resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView, "annotation.json")}
+    {:ok, resp, assign(socket, :video_id, video_id)}
   end
 
   def handle_in("new_annotation", params, socket) do
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -30,7 +30,10 @@ let Video = {
     }) 
 
     vidChannel.join()
-    .receive("ok", resp => console.log("joined the video channel", resp) )
+    .receive("ok", ({annotations}) => {
+      annotations.forEach( ann => this.renderAnnotation(msgContainer, ann))
+      console.log("joined the video channel") 
+    })
     .receive("error", reason => console.log("join failed", reason) )
                    
   },
