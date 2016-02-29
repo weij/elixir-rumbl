02ce81e9205b49fd2276ce6b8b6bb18604e19457
Weijun Yin <gw9812@gmail.com>
2016-02-29 16:02:43 +0800

    update video client when adding an annotation

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -1,26 +1,27 @@
 let Player = {
   player: null,
 
-  init(domId, playerId){
-    window.onYouTubeIframeAPIReady = () => this.onIframeReady(domId, playerId)
+  init(domId, playerId, onReady){
+    window.onYouTubeIframeAPIReady = () => {
+      this.onIframeReady(domId, playerId, onReady)
+    }
+      
     let youtubeScriptTag = document.createElement("script")
     youtubeScriptTag.src = "//www.youtube.com/iframe_api"
     document.head.appendChild(youtubeScriptTag)
   },
 
-  onIframeReady(domId, playerId){
+  onIframeReady(domId, playerId, onReady){
     this.player = new YT.Player(domId, {
       height: "360",
       width: "420",
       videoId: playerId,
       events: {
-        "onReady":  ( event => this.onPlayerReady(event) ),
+        "onReady":  ( event => onReady(event) ),
         "onStateChange": ( event => this.onPlayerStateChange(event) )
       }
     })
   },
-
-  onPlayerReady(event){ /* this.player.playVideo() */ },
 
   onPlayerStateChange(event){ },
   getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000) },
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -3,15 +3,20 @@ import Player from "./player"
 let Video = {
 
   init(socket, element){ if(!element){ return }
+    let videoId      = element.getAttribute("data-id")
+    let playerId     = element.getAttribute("data-player-id")
+    socket.connect()
+
+    Player.init(element.id, playerId, ()=> {
+      this.onReady(videoId, socket)
+    })
+  },  
+
+  onReady(videoId, socket){
     let msgContainer = document.getElementById("msg-container")
     let msgInput     = document.getElementById("msg-input")
     let postButton   = document.getElementById("msg-submit")
-    let videoId      = element.getAttribute("data-id")
-    let playerId     = element.getAttribute("data-player-id")
-    Player.init(element.id, playerId) 
-
-    socket.connect()
-    let vidChannel = socket.channel("videos:" + videoId)
+    let vidChannel = socket.channel("videos:" + videoId)  
 
     postButton.addEventListener("click", e => {
       let payload = {body: msgInput.value, at: Player.getCurrentTime()}
@@ -21,23 +26,30 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {         
-      this.renderAnnotation(msgContainer, resp)
-    })
+        this.renderAnnotation(msgContainer, resp)
+    }) 
 
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp) )
-      .receive("error", reason => console.log("join failed", reason) )
+    .receive("ok", resp => console.log("joined the video channel", resp) )
+    .receive("error", reason => console.log("join failed", reason) )
+                   
+  },
 
+  esc(str){
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
   },
 
   renderAnnotation(msgContainer, {user, body, at}){    
     let template = document.createElement("div")
     template.innerHTML = `
-    <b>${user.username}</b>: ${body}
+    <a href="#" data-seek="${this.esc(at)}">
+    <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
     `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
   }
-  
 }
 export default Video
